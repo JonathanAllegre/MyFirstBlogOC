a54d32e28190e463d0c3482e7fc0ccec98f355b2
Jonathan Allègre <jonathan.allegre258@orange.fr>
2018-03-31 11:45:37 +0200

    Refactoring HomeController

--- src/controller/frontend/HomeController.php
+++ src/controller/frontend/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AppController
 {
-    public function index(Session $session)
+    public function index()
     {
         $reponse = new Response($this->render('/front/Home/index.html.twig', [
             'name' => "Jonjon"
